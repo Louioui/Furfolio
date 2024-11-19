66867b7c6d93df1fdb0a45b0c9a46687772f70a4
luis pacheco <luispacheco13a@gmail.com>
2024-11-18 20:36:18 -0800

    Initial Commit

---
+++ Furfolio/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Furfolio
+//
+//  Created by mac on 11/18/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
